48fea274e83b02116c1d239658dc7f2004e1e252
Andrew <andrzej@wachalowicz.pl>
2023-06-18 15:19:54 +0200

    Próba modyfikacji

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,19 +1,2 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace HelloWorld
-{
-    //PascalCase
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //camelCase
-            string imie = "Andrew";
-
-            //UPPER_CASE
-            const double PI_NUMBER = 3.14;
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Jestem Andrew");
-        }
-    }
-}
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("I'm Andrew");
