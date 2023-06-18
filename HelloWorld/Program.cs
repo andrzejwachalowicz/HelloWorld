fe51da7eb0cc9166001df40cd525b795046895b6
Andrew <andrzej@wachalowicz.pl>
2023-06-18 14:57:55 +0200

    Dodaj pliki projektów.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    //PascalCase
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //camelCase
+            string imie = "Andrew";
+
+            //UPPER_CASE
+            const double PI_NUMBER = 3.14;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Jestem Andrew");
+        }
+    }
+}
